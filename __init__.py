598ade3c58d376c3540d4bbbb784b9f2b0c95bb4
hwolf <hannes@readyplayer.me>
2023-06-20 11:57:39 +0100

    addon packaging

--- __init__.py
+++ __init__.py
@@ -1,43 +1,9 @@
-"""
-small wrapper to make the validator available as a Blender addon
-"""
+# get file path and add to sys.path, so modules can be imported
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent))
 
-bl_info = {
-    "name": "Ready Player Me Validator",
-    "description": "Validate your Ready Player Me avatar in Blender",
-    "author": "Ready Player Me",
-    "version": (0, 0, 1),
-    "blender": (2, 91, 0),
-    "location": "Window/RPM Validator",
-    "category": "Development",
-}
-
-
-import bpy
-
-
-class OpenRPMValidator(bpy.types.Operator):
-    """Open the Ready Player Me Validator"""
-    bl_idname = "RPM.show_validator"
-    bl_label = "Open Ready Player Me Validator"
-    widget = None
-
-    def execute(self, context):
-        import rpm_validator
-        self.widget = rpm_validator.show()
-        return {'RUNNING_MODAL'}
-
-
-def menu_func(self, context):
-    """Add a menu item to the 'Window' menu"""
-    self.layout.operator(OpenRPMValidator.bl_idname)
-
-
-def register():
-    bpy.utils.register_class(OpenRPMValidator)
-    bpy.types.TOPBAR_MT_window.append(menu_func)
-
-
-def unregister():
-    bpy.utils.unregister_class(OpenRPMValidator)
-    bpy.types.TOPBAR_MT_window.remove(menu_func)
+# import addon code
+# addon code lives in a sep file, so we can also install it as a module,
+# and then run register() and unregister() from the module.
+from rpm_validator_addon import *
