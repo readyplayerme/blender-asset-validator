f97011906cdbc47862caf5310c1b6d0e49f5d5a0
TechyDaniel <117300186+TechyDaniel@users.noreply.github.com>
2023-07-11 13:59:57 +0200

    fix: names

--- rpm_pyblish_plugins/validate_object_armature.py
+++ rpm_pyblish_plugins/validate_object_armature.py
@@ -5,7 +5,7 @@ from rpm_pyblish_plugins.shared_funcs import get_mesh_by_name, object_from_mesh
 
 
 class ObjectArmature(pyblish.api.InstancePlugin):
-    """Pyblish plugin to validate the presence of an armature modifier on a mesh instance."""
+    """Validates the presence of an armature modifier on a mesh instance."""
 
     label = "Object Armature"
     version = (0, 1, 0)
@@ -27,4 +27,4 @@ class ObjectArmature(pyblish.api.InstancePlugin):
         obj = object_from_mesh(mesh)
 
         if obj.find_armature() is None:
-            self.log.error("Found %s with no armature modifier", mesh.name)
+            self.log.error("Found %s with no armature modifier", obj.name)
