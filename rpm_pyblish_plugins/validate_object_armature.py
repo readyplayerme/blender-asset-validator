450b82aee4a6a845d0dc5dac577c9e32b3526ebc
TechyDaniel <117300186+TechyDaniel@users.noreply.github.com>
2023-07-11 13:58:46 +0200

    fix: moved to object validation update : updated the configs misc: formatting

--- rpm_pyblish_plugins/validate_object_armature.py
+++ rpm_pyblish_plugins/validate_object_armature.py
@@ -4,23 +4,19 @@ from rpm_pyblish_plugins.action_plugin_openurl import OpenURL
 from rpm_pyblish_plugins.shared_funcs import get_mesh_by_name, object_from_mesh
 
 
-
-class MeshArmature(pyblish.api.InstancePlugin):
+class ObjectArmature(pyblish.api.InstancePlugin):
     """Pyblish plugin to validate the presence of an armature modifier on a mesh instance."""
 
-    label = "Mesh Armature"
+    label = "Object Armature"
     version = (0, 1, 0)
     order = pyblish.api.ValidatorOrder
     hosts = ["blender"]
     optional = False
-    families = ["Mesh"]
+    families = ["Object", "Rig"]
     match = pyblish.api.Subset
     actions = [OpenURL]
 
     url = "https://docs.readyplayer.me/asset-creation-guide/validation/validation-checks/mesh-validations/check-armature"  # noqa
-    skinned_meshes = const.SKINNED_MESHES
-
-
 
     def process(self, instance):
         """
@@ -29,12 +25,6 @@ class MeshArmature(pyblish.api.InstancePlugin):
 
         mesh = get_mesh_by_name(instance.name)
         obj = object_from_mesh(mesh)
-        modifiers = obj.modifiers
-        has_armature_modifier = any(
-            modifier.type == "ARMATURE" for modifier in modifiers
-        )
-        if mesh not in self.skinned_meshes and not has_armature_modifier :
-            self.log.error(
-                f"Found {mesh} with no armature modifier"
-            )
 
+        if obj.find_armature() is None:
+            self.log.error("Found %s with no armature modifier", mesh.name)
