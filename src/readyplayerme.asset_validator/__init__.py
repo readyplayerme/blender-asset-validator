4c4a326d753eee8a7a46c257bf44e5d9109b60f7
Olaf Haag <olaf@wolf3d.io>
2023-07-30 23:57:39 +0200

    fix!: use src-layout,  remove old addon stuff

---
+++ src/readyplayerme.asset_validator/__init__.py
@@ -0,0 +1,22 @@
+"""Blender addon to validate Ready Player Me assets."""
+
+bl_info = {
+    "name": "Ready Player Me Asset Validator",
+    "author": "Ready Player Me",
+    "description": "Validate your file data to match requirements for Ready Player Me assets.",
+    "blender": (3, 3, 0),
+    "version": (0, 3, 0),  # __version__ = '0.3.0' Do not remove this comment. Keep up-to-date, used by hatch.
+    "location": "",
+    "warning": "Requires installation of dependencies.",
+    "doc_url": "https://github.com/readyplayerme/blender-asset-validator#readme",
+    "tracker_url": "https://github.com/readyplayerme/blender-asset-validator/issues",
+    "category": "Ready Player Me",
+}
+
+
+def register():
+    ...
+
+
+def unregister():
+    ...
